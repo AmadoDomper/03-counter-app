0d86dcb288142502e8fe1f39f9751fc19e37d2bd
Amado Domper <amado.domper@gmail.com>
2022-03-17 09:49:26 -0500

    End of Section 4

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 //FC
-const PrimeraApp = () => {
-  const saludo = 'Hola Mundo'
+const PrimeraApp = ( { saludo , subtitulo } ) => {
 
   return (
     <>
-    <h1> {saludo }</h1>
+    <h1> { saludo }</h1>
       {/*<pre>{JSON.stringify( saludo ,null, 3)}</pre>*/}
-      <p>Mi primera aplicación</p>
+      <p> { subtitulo } </p>
     </>
   );
 };
+
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+  subtitulo: 'Soy un subtitulo'
+}
+
 
 export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react"
-import ReactDOM  from "react-dom"
-import PrimeraApp from "./PrimeraApp";
-
+import React from "react";
+import ReactDOM from "react-dom";
+// import PrimeraApp from "./PrimeraApp";
+import CounterApp from "./CounterApp";
 import './index.css';
-
 
 const divRoot = document.querySelector("#app");
 
-ReactDOM.render( <PrimeraApp />, divRoot);
+// ReactDOM.render(<PrimeraApp saludo="Hola Mundo" />, divRoot);
+ReactDOM.render(<CounterApp value={ 10 } />, divRoot);
