95dc187304e569bf357d560cbed7b0376c37c15c
AmadoDomper <amado.domper@gmail.com>
2022-03-20 17:57:30 -0500

    Add react tests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import PrimeraApp from "./PrimeraApp";
-import CounterApp from "./CounterApp";
+import PrimeraApp from "./PrimeraApp";
+// import CounterApp from "./CounterApp";
 import './index.css';
 
 const divRoot = document.querySelector("#app");
 
-// ReactDOM.render(<PrimeraApp saludo="Hola Mundo" />, divRoot);
-ReactDOM.render(<CounterApp value={ 10 } />, divRoot);
+ReactDOM.render(<PrimeraApp saludo="Hola, Soy Goku" />, divRoot);
+// ReactDOM.render(<CounterApp value={ 10 } />, divRoot);
