15b2123b11752cd482b38bf92d1245ca14aca4bb
Amado Domper <amado.domper@gmail.com>
2022-03-26 09:14:55 -0500

    Add test for click buttons

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -4,10 +4,14 @@ import { render } from "@testing-library/react";
 import CounterApp from '../CounterApp';
 
 describe('Pruebas en <CounterApp />', () => {
+    let wrapper = shallow(<CounterApp />);
+
+    beforeEach( () => {
+        wrapper = shallow(<CounterApp />);
+    })
+
     test('debe mostrar <CounterApp /> correctamente', () => {
         
-        const wrapper = shallow(<CounterApp />);
-
         expect(wrapper).toMatchSnapshot();
 
     });
@@ -19,11 +23,29 @@ describe('Pruebas en <CounterApp />', () => {
 
         const contador = wrapper.find('h2').text()*1;
 
-        console.log(contador)
+        // console.log(contador)
 
-        expect(contador).toBe(101);
+        expect(contador).toBe(100);
 
     })
+
+    test('debe de incrementar con el botón +1 ', () => {
+
+        wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        expect(counterText).toBe('11');
+
+    })
+
+    test('debe de decrementar con el botón -1 ', () => {
+
+        wrapper.find('button').at(2).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        console.log(counterText);
+        expect(counterText).toBe('9');
+
+    })
+
     
 })
  
